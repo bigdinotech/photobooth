2e0156c1ee911ae9ee4a93873863d494965e01a9
Leandro Lubrico <leandrolubrico@gmail.com>
2015-07-02 00:42:30 -0700

    Merge and Layout changes
    Merged files.
    Colors changed.

--- www/info.js
+++ www/info.js
@@ -1,21 +1,16 @@
 var timeoutTime = 8000;
 
 $(document).ready(function() {
-  var url = './takephotos.html?PHOTO=1';
+  var url = './takephotos.html?PHOTO=99';
   window.setTimeout(function() {
-    $('.info').fadeOut(2000, function() {
+    $('body').fadeOut(2000, function() {
       $(location).attr('href', url);
     });
   }, timeoutTime);
 
   $('html').click(function() {
-    $('.info').fadeOut(2000, function() {
+    $('body').fadeOut(2000, function() {
       $(location).attr('href', url);
     });
   });
 });
-
-function gotophotos() {
-  var url = './takephotos.html?PHOTO=99';
-  $(location).attr('href', url);
-}
