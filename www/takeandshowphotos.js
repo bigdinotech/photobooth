7cdb5dd5c3f9446083c95b491cf1757b39392bbe
Dino Tinitigan <bigdinotech@gmail.com>
2015-07-03 23:58:16 -0700

    Code cleanup
    Code cleanup + minor improvements

--- www/takeandshowphotos.js
+++ www/takeandshowphotos.js
@@ -65,44 +65,31 @@ function takePhoto(filename){
 	$.get(url.concat(filename)); //take photo
 }
 
-function takeFourPhotos(){
-	setTimeout(function() {
-		console.log("image0");
-		document.getElementById('imageframe0').contentWindow.document.location.href="countdown.html";
-		setTimeout(function() {
-			var filename = 'image0.jpg';
-			takePhoto(filename);
-			document.getElementById('imageframe0').contentWindow.document.location.href="image0.php";
-		}, 5000);
-		setTimeout(function() {
-			console.log("image1");
-			document.getElementById('imageframe1').contentWindow.document.location.href="countdown.html";
-			setTimeout(function() { 
-				var filename = 'image1.jpg';
-				takePhoto(filename);
-				document.getElementById('imageframe1').contentWindow.document.location.href="image1.php";
-			}, 5000);
+function takePhotos(){
+	var photoCount = 4;
+	var imageID = 0;
+	(function photoLoop (photoCount, imageID) {
+		var filename = "image";
+		var iframe = "imageframe";
+		var imageLoader = "image";
+		setTimeout(function () {   
+			filename = "image" + imageID + ".jpg";
+			iframe = 'imageframe' + imageID;
+			document.getElementById(iframe).contentWindow.document.location.href="countdown.html";
 			setTimeout(function() {
-				console.log("image2");
-				document.getElementById('imageframe2').contentWindow.document.location.href="countdown.html";
-				setTimeout(function() {
-					var filename = 'image2.jpg';
-					takePhoto(filename);
-					document.getElementById('imageframe2').contentWindow.document.location.href="image2.php";
-				}, 5000);
-				setTimeout(function() {
-					console.log("image3");
-					document.getElementById('imageframe3').contentWindow.document.location.href="countdown.html";
-					setTimeout(function() {
-						var filename = 'image3.jpg';
-						takePhoto(filename);
-						document.getElementById('imageframe3').contentWindow.document.location.href="image3.php";
-						document.getElementById('instructiontr').style.visibility = "visible";
-					}, 5000);
-				}, 15000);
-			}, 15000);
-		}, 15000);
-	}, 15000);
+				takePhoto(filename);
+				imageLoader = "image" + (imageID-1) + ".php";
+				document.getElementById(iframe).contentWindow.document.location.href=imageLoader;
+			}, 5000);
+			imageID++;
+		  if (--photoCount) photoLoop(photoCount, imageID);
+		  if(photoCount == 0)
+		  {
+			document.getElementById('instructiontr').style.visibility = "visible";
+			enableRetake();
+		  }
+	   }, 15000)
+	})(photoCount, imageID);
 }
 
 function takeanddisplayphoto(index) {
@@ -114,18 +101,35 @@ function takeanddisplayphoto(index) {
 	takePhoto(fname);
 	document.getElementById(imageFrame).contentWindow.document.location.href=imageLoader
 }
-function displayPlaceholders() {
+
+function initializePhotoGrid() {
 	document.getElementById('imageframe0').contentWindow.document.location.href="imageplaceholder.php";
 	document.getElementById('imageframe1').contentWindow.document.location.href="imageplaceholder.php";
 	document.getElementById('imageframe2').contentWindow.document.location.href="imageplaceholder.php";
 	document.getElementById('imageframe3').contentWindow.document.location.href="imageplaceholder.php";
-}
-$(document).ready(function(){
 	document.getElementById('instructiontr').style.visibility = "hidden";
-	displayPlaceholders();
+	disableRetake();
+}
+
+function disableRetake()
+{
+	document.getElementById('iframeContainer0').style.pointerEvents = 'none';
+	document.getElementById('iframeContainer1').style.pointerEvents = 'none';
+	document.getElementById('iframeContainer2').style.pointerEvents = 'none';
+	document.getElementById('iframeContainer3').style.pointerEvents = 'none';
+}
+
+function enableRetake()
+{
+	document.getElementById('iframeContainer0').style.pointerEvents = 'all';
+	document.getElementById('iframeContainer1').style.pointerEvents = 'all';
+	document.getElementById('iframeContainer2').style.pointerEvents = 'all';
+	document.getElementById('iframeContainer3').style.pointerEvents = 'all';
+}
+
+$(document).ready(function(){
+	initializePhotoGrid();
 	setTimeout(function() {
-		takeFourPhotos();
-	}, 1000);
-	console.log("done 4 photos?");
-    
+		takePhotos();
+	}, 1000); 
 });
