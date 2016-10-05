3589af270b77cbeb12104a9b5926fb68fb943875
Dino Tinitigan <dino.tinitigan@intel.com>
2016-10-04 23:12:13 -0700

    Add a separate script for each layout
    -add assemblelogo and assemblebanner scripts
    -remove old unsued scripts

--- www/assembleLabel.php
+++ www/assembleLabel.php
@@ -3,22 +3,6 @@ $filename = $_GET["photofilename"];
 //$labelname = $_GET["labelfilename"];
 $labelfilename = "./images/label.jpg";
 
-//do some cleanup
-$command = "rm " . "temp_montage2.jpg";
-exec($command);
-$command = "rm " . $filename;
-exec($command);
-
-//assemble image
-//TODO: resize only if file sizes seems to big
-$command = "mogrify -resize 900x600 image*.jpg";
-exec($command);
-$command = "montage image*.jpg -tile 2x2 -geometry +5+5 " . "temp_montage2.jpg"; 
-exec($command);
-$command = "mogrify -resize 1280x720 "  . "temp_montage2.jpg";
-exec($command);
-$command = "convert temp_montage2.jpg " . "-bordercolor white -border 100x0 " . "temp_montage2.jpg";
-exec($command);
-$command = "convert temp_montage2.jpg +append " . $labelfilename . " -append " . $filename;
+$command = "filename=" . $filename . " labelfilename=" . $labelfilename . " ./scripts/assemblebanner";
 exec($command);
 ?>
--- www/assembleLogo.php
+++ www/assembleLogo.php
@@ -1,14 +1,8 @@
 <?php
 $filename = $_GET["photofilename"];
+//$logoname = $_GET["logofilename"];
 $logoname = "./images/logo.jpg";
 
-//do some cleanup
-$command = "rm " . $filename;
-exec($command);
-
-//assemble image
-$command = "mogrify -resize 900x600 image*.jpg";
-exec($command);
-$command = "montage image*.jpg " . $logoname . " -tile 2x2 -geometry +5+5 " . $filename;
+$command = "filename=" . $filename . " logoname=" . $logoname . " ./scripts/assemblelogo";
 exec($command);
 ?>
